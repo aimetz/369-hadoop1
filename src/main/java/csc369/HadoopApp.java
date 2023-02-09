4d7e7feb30cee77eea002d6ba8721b41082f8c6a
aimetz <aimetz@calpoly.edu>
2023-02-09 14:59:45 -0800

    Accesslog 5

--- src/main/java/csc369/HadoopApp.java
+++ src/main/java/csc369/HadoopApp.java
@@ -56,19 +56,24 @@ public class HadoopApp {
 			System.exit(-1);
 		}
 
-			FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
-			FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+		FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
 
-			job.waitForCompletion(true);
-		Configuration conf2 = new Configuration();
-		Job sortValues = new Job(conf2, "Sort Values");
-		if ("AccessLog1".equalsIgnoreCase(otherArgs[0])) {
+		job.waitForCompletion(true);
+
+		if (otherArgs[0].contains("1") || otherArgs[0].contains("4")) {
+			Configuration conf2 = new Configuration();
+			Job sortValues = new Job(conf2, "Sort Values");
+			FileInputFormat.addInputPath(job, new Path(otherArgs[2]));
+			FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]+"b"));
+
 			sortValues.setReducerClass(AccessLog8.ReducerImpl.class);
 			sortValues.setMapperClass(AccessLog8.MapperImpl.class);
 			sortValues.setOutputKeyClass(AccessLog8.OUTPUT_KEY_CLASS);
 			sortValues.setOutputValueClass(AccessLog8.OUTPUT_VALUE_CLASS);
+
+			System.exit(sortValues.waitForCompletion(true) ? 0 : 1);
 		}
-		System.exit(sortValues.waitForCompletion(true) ? 0: 1);
 	}
 
 }
